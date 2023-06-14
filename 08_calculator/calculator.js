3e7baba8fc23870ce5903456f04bd3033e31da83
myaooooo98 <yaoer0505@gmail.com>
2023-06-14 20:51:39 +0800

    first commit

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	array.forEach(ele => {
+    sum += ele;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 0;
+  array.forEach(ele => {
+    result *= ele;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ^ b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 0;
+	for (let i = 1; i >= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
